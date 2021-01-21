61fceb8370f12cf49b8b648becc6484c7083ce1f
Adrian velasquez Guerrero <adrian.velasquez.guerrero@gmail.com>
2021-01-20 19:30:36 -0500

    Tipos de datos

---
+++ main.js
@@ -0,0 +1,37 @@
+// Tipos de datos 
+
+// String cadena
+
+"Adrian "
+'Velasquez Guerrero'
+
+// Numero
+
+1000
+2.5
+-25
+
+//Boolean
+
+true
+false
+
+
+//array listas
+
+['Adrian','Velasquez','Guerrero']
+
+[1,2,3,4]
+
+[true, false]
+
+
+// Objetos
+{
+   nombre: 'Adrian',
+   apellido: 'Velasquez',
+   edad     : 50,
+   profesor : false
+   antiguedad : false
+
+}
